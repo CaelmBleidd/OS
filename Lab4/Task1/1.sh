d0eb67d9f872ce6845d62ae33ece0331c3f25624
caelmbleidd <alex.menshutin99@gmail.com>
2019-05-08 10:45:23 +0300

    Minor changes and bug fixes

--- Lab4/Task1/1.sh
+++ Lab4/Task1/1.sh
@@ -1,14 +1,17 @@
 #!/bin/bash 
 TESTDIR="$HOME/Programming/OS/Lab4/Task1/test"
 REPORT_FILE="$HOME/Programming/OS/Lab4/report"
-WEBSITE='www.net_nikogo.ru'
+WEBSITE='www.ya.ru'
 
 date=$(echo $(date) | sed "s/ /-/g")
+
+rm -r $TESTDIR
+rm ../report
 
 mkdir $TESTDIR && 
     {
         echo "catalog has been created successfully" > $REPORT_FILE && 
         echo > "$TESTDIR/$date"
-    }
-ping $WEBSITE || echo "couldn't reach $WEBSITE" >> $REPORT_FILE
+}
+(ping $WEBSITE -c4 &&  echo "$WEBSITE has been reached" >> $REPORT_FILE) || echo "couldn't reach $WEBSITE" >> $REPORT_FILE
 
--- Lab4/Task3/3.sh
+++ Lab4/Task3/3.sh
@@ -1,2 +1,5 @@
-crontab -l
+crontab -l > tmp_file
+echo "*/5 * * * 4 $HOME/Programming/OS/Lab4/Task1/1.sh" >> tmp_file
+crontab tmp_file
+rm tmp_file
 
